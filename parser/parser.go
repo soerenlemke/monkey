3d27eaba9ddab8f7f639ce1e323d8eb415e1339c
soerenlemke <soerenlemke@gmx.de>
2025-09-15 16:20:52 +0200

    preparing the ast page 52

--- parser/parser.go
+++ parser/parser.go
@@ -91,6 +91,7 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	stmt := &ast.ReturnStatement{Token: p.curToken}
 	p.nextToken()
+
 	// TODO: We're skipping the expressions until we
 	// encounter a semicolon
 	for !p.curTokenIs(token.SEMICOLON) {
